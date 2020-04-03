3ff09f270778c8e0404734181da316d6437d287e
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-04-03 19:58:47 +0100

    added

--- math.js
+++ math.js
@@ -1,8 +1,10 @@
 const multi = (a, b) => (a * b)
 const div = (a, b) => (a / b)
 
+const numbers = [1, 2, 3, 4, 5]
+
 // pattarn1 best: export as module 
-module.exports = {multi, div}
+module.exports = {multi, div, numbers}
 
 //// pattarn2 better: export with properties 
 // exports.func = {multi, div}
@@ -10,3 +12,5 @@ module.exports = {multi, div}
 //// pattarn3 worst: export with properties 
 // exports.div = div
 // exports.multi = multi
+
+
--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
-// const assert = require('assert');
+const assert = require('chai').assert;
 const expect = require('chai').expect;
+const should = require('chai').should();
 //pattern 1
 const math = require('../math.js'); 
 ////pattern2
@@ -8,11 +9,52 @@ const math = require('../math.js');
 // const math = require('../math.js')
 
 
-describe('正常系', () => {
-	it('掛け算', () => {
-        expect(math.multi(2,3)).to.equal(6, '戻り値は6');
+//// BDD expect
+describe('計算確認 with expect', () => {
+	it('掛け算 expect', () => {
+        expect(math.multi(2, 3)).to.equal(6, '戻り値は6');
     });
-    it('割り算', () => {
-        expect(math.div(6,3)).to.equal(2, '戻り値は2');
+    it('割り算 expect', () => {
+        expect(math.div(6, 3)).to.equal(2, '戻り値は2');
 	});
 });
+
+////BDD should
+describe('計算確認 with should', () => {
+    it('掛け算 should', () => {
+        math.multi(2, 3).should.equal(6,'戻り値は6')
+    });
+    it('割り算 should', () => {
+        math.div(6, 3).should.equal(2,'戻り値は2')
+    });
+});
+
+//// TDD
+// suite('計算確認　with assert', ()=> {
+//       test('掛け算 assert', () => {
+//         assert.equal(6, math.multi(2, 3), '戻り値は6');
+//       });
+//       test('割り算 assert', () => {
+//         assert.equal(2, math.div(6, 2), '戻り値は3');
+//       });
+// });
+
+// vanilla TDD 
+assert.equal(math.multi(2,  3), 6, '戻り値は6');
+assert.equal(math.div(6,  3), 2, '戻り値は2');
+
+
+
+//////Array Test
+//// BDD expect
+expect(math.numbers).to.be.an('array').that.includes(2);
+expect(math.numbers).to.have.lengthOf(5);
+
+//// BDD should
+math.numbers.should.be.an('array').that.includes(2);
+math.numbers.should.have.lengthOf(5);
+
+//// TDD
+assert.isArray(math.numbers, 'is array of numbers');
+assert.include(math.numbers, 2, 'array contains 2');
+assert.lengthOf(math.numbers, 5, 'array contains 5 numbers');
