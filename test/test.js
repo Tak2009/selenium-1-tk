39aa7adc20098e90736db6c6ee26562c7aab29cf
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-04-03 11:51:50 +0100

    changed

--- test/test.js
+++ test/test.js
@@ -1,2 +1,8 @@
 const assert = require('assert');
 const testFile = require('./test.js');
+
+describe('正常系', function() {
+	it('2, 3', function() {
+		expect(multi(2,3)).to.equal(6, '戻り値は6');
+	});
+});
