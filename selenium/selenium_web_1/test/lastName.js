e3dc57bbccc5c444eec61f3b6274d2c73ebe8ce2
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-04-08 21:56:00 +0100

    changed

--- selenium/selenium_web_1/test/lastName.js
+++ selenium/selenium_web_1/test/lastName.js
@@ -1,5 +1,5 @@
 // import the selenium web driver
-const webdriver = require('selenium-webdriver');
+const { Builder, Key, By } = require("selenium-webdriver");
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const should = require('chai').should();
@@ -8,18 +8,37 @@ let driver;
 
 describe("Last Name", () => {
   before(() => {
-    driver = new webdriver.Builder().forBrowser("chrome").build();
+    driver = new Builder().forBrowser("chrome").build();
   });
 
   after(() => {
     return driver.quit();
   });
 
-  it("check if this field is not blank", async () => {
+  it("blank case", async () => {
     await driver.get("https://revolgy-forms-case-study-master.staging.axiory.com/jp/registration/demo");
     // 画面サイズ
     await driver.manage().window().setRect(1440, 830)
     // いったんinput areaにはいる
     await driver.findElement(By.id("lastname")).click()
+    await driver.findElement(By.id("firstname")).click()
+    await driver.findElement(By.css(".atm-error-message-container")).click()
+    assert(await driver.findElement(By.css(".atm-error-message-container")).getText() == "お客様のご苗字をご教示下さい。")
   });
+  it("number case", async () => {
+    // いったんinput areaにはいる
+    await driver.findElement(By.id("lastname")).sendKeys("9")
+    await driver.findElement(By.id("firstname")).click()
+    await driver.findElement(By.css(".atm-error-message-container")).click()
+    assert(await driver.findElement(By.css(".atm-error-message-container")).getText() == "ご苗字が正しくないフォーマットで記入されています。半角の英字でご記入下さい。")
+  });
+  it("pass case. erorr message should be empty", async () => {
+    // いったんinput areaにはいる
+    await driver.findElement(By.id("lasttname")).click()
+    await driver.findElement(By.id("lasttname")).clear()
+    await driver.findElement(By.id("lastname")).sendKeys("kimotoKIMOTO")
+    await driver.findElement(By.id("firstname")).click()
+    const errorMessage = await driver.findElement(By.css(".atm-error-message-container")).getText()
+    assert.equal(errorMessage, "");
+    });
 });
